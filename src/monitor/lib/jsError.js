0ab526efbbb46d1636369f6efc9ca315f793557e
潘静怡 <1003346758@qq.com>
2023-09-28 18:33:05 +0800

    resource error

--- src/monitor/lib/jsError.js
+++ src/monitor/lib/jsError.js
@@ -3,28 +3,37 @@ import getSelector from '../utils/getSelector'
 import tracker from '../utils/tracker'
 
 export function injectJsError() {
-  //* 监控js异常 + 监控资源加载异常 监听全局未捕获的错误
+  //* 监控js异常 + 监控资源加载异常
   window.addEventListener('error', function(event){ //event:错误事件对象，该对象上有所有相关信息，可以打印出来看一下
     let lastEvent = getLastEvent() //最后一个交互事件
     // lastEvent 这个对象中有一个path属性，path属性值：body div#container div.content input
 
-    let log = { //定义日志格式
-      kind: 'stability', //监控指标的大类(这里的意思是稳定性指标)
-      type: 'error', //小类型 这是一个错误
-      errorType: 'jsError', //JS执行错误
-      url: '', //访问哪个路径 报错了
-      message: event.message, //报错信息
-      filename: event.filename, //哪个文件报错了
-      position: `${event.lineno}: ${event.colno}`, //报错位置：行列
-      stack: getLines(event.error.stack), //报错堆栈，getLines函数处理格式
-      selector: lastEvent ? getSelector(lastEvent.path) : '', //代表最后一个操作的元素, 即事件类型('click','pointerdown', 'touchstart', 'mousedown', 'keydown', 'mouseover')
+    if (event.target && (event.target.src || event.target.href)) {
+      tracker.send({//*资源加载错误
+        kind: 'stability',//稳定性指标
+        type: 'error',//resource
+        errorType: 'resourceError',
+        filename: event.target.src || event.target.href,//加载失败的资源
+        tagName: event.target.tagName,//标签名
+        timeStamp: formatTime(event.timeStamp),//时间
+        selector: getSelector(event.path || event.target),//选择器
+      })
+    } else { //* js错误
+      tracker.send({ //定义日志格式
+        kind: 'stability', //监控指标的大类(这里的意思是稳定性指标)
+        type: 'error', //小类型 这是一个错误
+        errorType: 'jsError', //JS执行错误
+        url: '', //访问哪个路径 报错了
+        message: event.message, //报错信息
+        filename: event.filename, //哪个文件报错了
+        position: `${event.lineno}: ${event.colno}`, //报错位置：行列
+        stack: getLines(event.error.stack), //报错堆栈，getLines函数处理格式
+        selector: lastEvent ? getSelector(lastEvent.path) : '', //代表最后一个操作的元素, 即事件类型('click','pointerdown', 'touchstart', 'mousedown', 'keydown', 'mouseover')
+      }) //将 log 发送到 阿里云日志监控
     }
+  }, true) //true代表在捕获阶段调用,false代表在冒泡阶段捕获,使用true或false都可以
 
-    console.log('监控报错信息',log)
-    tracker.send(log) //将 log 发送到 阿里云日志监控
-  })
-
-  //* 捕获 promise 异常
+  //* 捕获 promise 异常: 当Promise 被 reject 且没有 reject 处理器的时候，会触发 unhandledrejection 事件
   window.addEventListener('unhandledrejection', function (event) {
     let lastEvent = getLastEvent();
     let message = '';
@@ -60,7 +69,7 @@ export function injectJsError() {
         selector: lastEvent ? getSelector(lastEvent.path || lastEvent.target) : ''
     })
   }, true);// true代表在捕获阶段调用,false代表在冒泡阶段捕获,使用true或false都可以
-
+}
 
 function getLines(stack) {
   if (!stack) {
